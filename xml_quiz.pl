c3d89a6811a1e7f4d101b7ab7b9ac1d5bc3df592
Sophie Rudd <sophie@The-Linux-Box.default>
2017-01-16 19:33:36 +0000

    More work

--- xml_quiz.pl
+++ xml_quiz.pl
@@ -74,12 +74,66 @@ sub run_header{
 	$_[1] = join("\n",$_[1],"\\end\{abstract\}\n\\begin\{document\}\n\\maketitle");
 }
 
+sub validate_num{
+	my @num = $_[0]->getElementsByTagName("number");
+	my $num = $num[0]->textContent;
+	die "Toss-up $num is incorrectly numbered." unless $num == $_[2] + 1 or $_[2] <= $_[3];
+	$_[2] = $num;
+	$_[1] = join("\n",$_[1],"$num.");
+	$num;
+}
+
 #subroutine to process the toss-up tags
 sub run_tu{
+	my $num = &validate_num($_[0],$_[1],$_[4],$_[5]);
+	my @points = $_[0]->getElementsByTagName("points");
+	if (scalar @points == 0) {
+		die "No value set for points in toss-up $num." unless defined $_[2];
+	} else {
+		my $points_txt = $points[0]->textContent;
+		$_[1] = join("",$_[1],"  $points_txt points.");
+	}
+	my @q = $_[0]->getElementsByTagName("question");
+	&run_tu_q($q[0],$_[1],$_[3]);
+	my @ans = $_[0]->getElementsByTagName("answer");
+	&run_ans($ans[0],$_[1]);
+}
+
+sub run_inst{
+	my @inst = $_[0]->getElementsByTagName("instructions");
+	$_[1] = join("\n",$_[1],$inst[0]->textContent) if scalar @inst > 0;
+}
+
+sub run_tu_q{
+	&run_inst($_[0],$_[1]);
+	my @text = $_[0]->getElementsByTagName("text");
+	$_[1] = join("\n",$_[1],$text[0]->textContent);
+}
+
+sub run_ans{
+	&run_inst($_[0],$_[1]);
+	my @al = $_[0]->getElementsByTagName("al");
+	&run_al($al[0],$_[1]);
+}
+
+sub run_al{
+	my @la = $_[0]->getElementsByTagName("la");
+	$_[1] = join("\n",$_[1],"");
+	foreach my $la (@la){
+		$_[1] = join(", ",$_[1],$la->textContent);
+	}
 }
 
 #subroutine to process the bonus set tags
 sub run_bs{
+	my $num = &validate_num($_[0],$_[1],$_[4],$_[5]);
+	my @points = $_[0]->getElementsByTagName("points_per_bonus");
+	if (scalar @points == 0){
+		die "No value set for points in bonus set $num." unless defined$_[2];
+	} else {
+		my $ppb = $points[0]->textContent;
+		$_[1] = join("\n",$_[1],"$ppb points per bonus.");
+	}
 }
 
 
@@ -111,30 +165,29 @@ my $power_points;
 #find and evaluate header tag
 my @h_tag = $dom->documentElement()->getChildrenByTagName("header");
 &run_header($h_tag[0],$outp,$toss_ups,$bonus_sets,$tblink,$tu_points,$bonus_points,$boni_per_set,$description,$power_points);
-
 #find toss-up and bonus tags (if any)
 my @tu_tags = $dom->documentElement()->getChildrenByTagName("toss_up");
 my @bs_tags = $dom->documentElement()->getChildrenByTagName("bonus_set");
 
 #evaluate tu and bonus tags
+my $qnum = 0;
 if ($tblink eq "false"){
 	foreach my $tu_tag (@tu_tags){
-		&run_tu($tu_tag,$outp,$tu_points,$power_points);
+		&run_tu($tu_tag,$outp,$tu_points,$power_points,$qnum,$toss_ups);
 	}
+	$qnum = 0;
+	$outp = join("\n",$outp,"Bonus sets.");
 	foreach my $bs_tag (@bs_tags){
-		&run_bs($bs_tag,$outp,$bonus_points,$boni_per_set);
+		&run_bs($bs_tag,$outp,$bonus_points,$boni_per_set,$qnum,$bonus_sets);
 	}
 } else {
 	my $acc = 0;
 	while($acc < ($toss_ups >= $bonus_sets ? $toss_ups : $bonus_sets)){
-		&run_tu($tu_tags[$acc],$outp,$tu_points,$power_points) if $acc < scalar @tu_tags;
-		&run_bs($bs_tags[$acc],$outp,$bonus_points,$boni_per_set) if $acc < scalar @bs_tags;	
+		&run_tu($tu_tags[$acc],$outp,$tu_points,$power_points,$qnum,$toss_ups) if $acc < scalar @tu_tags;
+		&run_bs($bs_tags[$acc],$outp,$bonus_points,$boni_per_set,$qnum,$bonus_sets) if $acc < scalar @bs_tags;
+		$qnum++;
 	}
 }
 $outp = join("\n",$outp,"\\end\{document\}\n");
 say $outp;
-my @qs = $dom->documentElement()->getChildrenByTagName("toss_up")->get_node(2)->getChildrenByTagName("question")->get_node(1)->getChildrenByTagName("text")->get_node(1);
-
-say $qs[0];
-say $qs[1];
 
